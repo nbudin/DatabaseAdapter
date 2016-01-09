4b621757d8125b7c7c8b1dc7fe9bce753aa8278d
Nat Budin <nbudin@patientslikeme.com>
2016-01-09 10:03:26 -0500

    Initial Commit

---
+++ DatabaseAdapter/DatabaseAdapter.h
@@ -0,0 +1,19 @@
+//
+//  DatabaseAdapter.h
+//  DatabaseAdapter
+//
+//  Created by Nat Budin on 1/9/16.
+//  Copyright © 2016 Nat Budin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DatabaseAdapter.
+FOUNDATION_EXPORT double DatabaseAdapterVersionNumber;
+
+//! Project version string for DatabaseAdapter.
+FOUNDATION_EXPORT const unsigned char DatabaseAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DatabaseAdapter/PublicHeader.h>
+
+
